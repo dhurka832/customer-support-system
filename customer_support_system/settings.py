2c3c68a4930c637ddb57922251ee2f5ac3c8ecb8
dhurka832 <dhurka832@gmail.com>
2026-07-17 14:14:02 +0530

    add postgresql changes

--- customer_support_system/settings.py
+++ customer_support_system/settings.py
@@ -86,8 +86,9 @@ WSGI_APPLICATION = 'customer_support_system.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        default=os.getenv("DATABASE_URL"),
         conn_max_age=600,
+        ssl_require=True,
     )
 }
 
