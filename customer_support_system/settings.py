06dd0ae6759683f1be7e3950f2679ef3ee48f129
dhurka832 <dhurka832@gmail.com>
2026-07-17 21:10:07 +0530

    set allowed hosts

--- customer_support_system/settings.py
+++ customer_support_system/settings.py
@@ -31,7 +31,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "customer-support-system-3hyq.onrender.com",
+]
 
 
 # Application definition
